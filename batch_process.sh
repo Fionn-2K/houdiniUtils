b887cbd6e48395fb91a50d737a531037bdcfb63b
Fionn-2K <fionn.sherrard@2k.com>
2024-03-05 16:46:58 +0000

    fixes
    batch_process.h ran from Houdini's hcmd (open shell, run hcmd, set path the folder containing shell command)
    fix to script.py
    -wrog ref for export_file node

--- batch_process.sh
+++ batch_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for filename in C:/Users/fionn.sherrard/Downloads/assets/obj_test/*.obj; do
-  hython script.py "D:\Rebelway\Week_4\Pipeline_template_example.hip" "$filename"
+  hython script.py "D:/Rebelway/Week_4/Pipeline_template_example.hip" "$filename"
 done
--- script.py
+++ script.py
@@ -14,6 +14,6 @@ file_node.parm("file").set(input_file)
 name_node = hou.node("/obj/geo1/set_name") # attribute wrangler node in Houdini project
 name_node.parm("name").set(input_file)
 
-export_node = hou.node("/obj/geo1/set_name")
+export_node = hou.node("/obj/geo1/filecache1")
 export_node.parm("file").set(input_file + "remeshed.bgeo.sc")
 export_node.parm("execute").pressButton()
