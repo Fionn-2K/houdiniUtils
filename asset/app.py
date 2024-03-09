fdb6b5b4dcd88a3542fb1cba96b67ef9b11b8729
Fionn-2K <fionn.sherrard@2k.com>
2024-03-09 14:51:59 +0000

    Flask intro

---
+++ asset/app.py
@@ -0,0 +1,29 @@
+import json
+from flask import Flask, request, render_template
+app = Flask("3d Asset")
+
+## Decorator
+@app.route('/submit', methods=['POST'])
+def submit():
+    ## When "Submit" is pressed get data from fields
+    data = {
+        "name": request.form["name"],
+        "polycount": request.form["polycount"],
+        "id": request.form["id"],
+        "user": request.form["user"]
+    }
+    ## write data to json file
+    with open("asset_data.json", "w") as json_file:
+        json.dump(data, json_file)
+
+    return "Data saved correctly"
+
+@app.route('/')
+def index():
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+## Running app.py with the Flask command will open a development server.
+## When "Submit" is press the data in the displayed fields will be used to generate a json file on the server.
